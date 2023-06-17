35cdca9f9de888a0c6ae08f963306febcbe473ac
luarguedas <luceroyaninaarguedasescalante@gmail.com>
2023-06-17 10:59:07 -0500

    primer commit

---
+++ src/main/java/com/isil/isilnet/Dae7Application.java
@@ -0,0 +1,13 @@
+package com.isil.isilnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dae7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dae7Application.class, args);
+	}
+
+}
